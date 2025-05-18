75601d9f86390589b2ee831c18bf060973906612
Clebson-Lira <cpireslira@gmail.com>
2025-05-18 20:33:59 -0300

    criação do cors para o backend

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import dotenv from 'dotenv';
 import 'reflect-metadata';
 import express from 'express';
-
+import cors from 'cors';
 import { AppDataSource } from './config/data-source';
 import authRoutes from './routes/auth';
 import accountRoutes from './routes/account';
@@ -13,6 +13,12 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+app.use(cors({
+  origin: 'http://localhost:4200',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 const PORT = process.env.PORT || 3000;
 
