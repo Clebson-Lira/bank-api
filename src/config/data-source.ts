da2916abb767dfe1236f9468f23b181fd082b95b
Clebson <clebson.lira@essentia.com.br>
2025-05-28 12:45:39 -0300

    feat: adicionar logger com winston e inicialização do DataSource

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,18 +1,38 @@
 import { DataSource } from 'typeorm';
-import { User } from '../entities/User';
-import { Account } from '../entities/Account';
-import { Transaction } from '../entities/Transaction';
+import path from 'path';
+import winston from 'winston';
+
+const logger = winston.createLogger({
+  level: 'error',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.Console()
+  ],
+});
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3306,
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || 'Abc123456',
-  database: process.env.DB_NAME || 'banking_system',
+  host: process.env.DB_HOST ,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   synchronize: false,
   logging: false,
-  entities: [User, Account, Transaction],
+  entities: [ __dirname + '/entities/*.{js,ts}',
+        path.join(__dirname, '..', 'modules', '**', '*.entity.{js,ts}'),],
   migrations: ['src/migrations/*.ts'],
   subscribers: [],
 });
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    logger.error('Erro ao inicializar o Data Source', { error: err });
+  });
